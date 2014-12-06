51a6a2725923083333f0c58ef8b317fc68684b80
cash <cash@cashdeMacBook-Pro.local>
2014-12-06 15:11:15 +0800

    Initial Commit

---
+++ AllTest/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AllTest
+//
+//  Created by cash on 14-12-6.
+//  Copyright (c) 2014年 ___CASH___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
